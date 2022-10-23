2fce4518f5851812df6161ef5a67a778aca17bc7
Osbertt <minhtetnaing25mhn@gmail.com>
2022-10-23 14:19:18 +0630

    Added logo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import Todo from "./Components/TodoList";
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    showTodo: true,
-  };
-  handleToggle = (e) => {
-    this.setState({ showTodo: !this.state.showTodo });
-  };
   render() {
     return (
       <div className="App">
--- src/Components/Todo Components/FilterTodo.js
+++ src/Components/Todo Components/FilterTodo.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-export default class FilterTodo extends React.Component{
-    render(){
-        return <div className="filter">
-            <button onClick={()=>this.props.handleFilter("all")}>All</button>
-            <button onClick={()=>this.props.handleFilter("active")}>Active</button>
-            <button onClick={()=>this.props.handleFilter("complete")}>Complete</button>
-            <button onClick={this.handleDeleteComplete}>Delete Complete</button>
-
-        </div>
-    }
+export default class FilterTodo extends React.Component {
+  render() {
+    return (
+      <div className="filter">
+        <button onClick={() => this.props.handleFilter("all")}>All</button>
+        <button onClick={() => this.props.handleFilter("active")}>
+          Active
+        </button>
+        <button onClick={() => this.props.handleFilter("complete")}>
+          Complete
+        </button>
+        <button onClick={this.props.handleDeleteComplete}>
+          Delete Complete
+        </button>
+      </div>
+    );
+  }
 }
